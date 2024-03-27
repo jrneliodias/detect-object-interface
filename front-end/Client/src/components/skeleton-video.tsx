2dfab31e40c7ad5e682e7ecc46c6c5c5e28a0f2f
Nélio Dias <jrneliodias@gmail.com>
2024-03-27 19:50:14 -0300

    feat: create skeleton component

---
+++ front-end/Client/src/components/skeleton-video.tsx
@@ -0,0 +1,10 @@
+import { Skeleton } from "@/components/ui/skeleton"
+
+export function SkeletonVideo() {
+    return (
+
+        <Skeleton className=" min-h-[360px] h-fit min-w-[200px] w-1/2" />
+
+
+    )
+}
