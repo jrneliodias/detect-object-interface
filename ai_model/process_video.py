c891b5b29281da4fef7b616bf5631ce36c984926
Nélio Dias <jrneliodias@gmail.com>
2024-03-29 15:06:54 -0300

    feat: ensure that detectionProcess object store the detections data

--- ai_model/process_video.py
+++ ai_model/process_video.py
@@ -10,7 +10,7 @@ import json
 model = Model("yolov8s")
 
 
-class Detections:
+class DetectionsProcess:
     def __init__(self) -> None:
         self.all_detections = []
 
@@ -18,7 +18,7 @@ class Detections:
         if not new_detection_array:
             return
         for detection in new_detection_array:
-            detection["frame"] = frame_number
+            detection["frame_number"] = frame_number
         self.insert_detections_to_all_detections(new_detection_array)
 
     def insert_detections_to_all_detections(self, new_detection_array: list[dict]):
@@ -34,9 +34,11 @@ class Detections:
 
 class VideoProcessor:
     def __init__(self,
+                 all_detections: DetectionsProcess,
                  video_path: str,
                  confidence: float = 0.7,
-                 iou: float = 0.5) -> None:
+                 iou: float = 0.5,
+                 ) -> None:
 
         self.input_video_path = video_path
         self.output_video_path = ''
@@ -48,6 +50,7 @@ class VideoProcessor:
         self.cap = ''
         self.out = ''
         self.frame_number = 1
+        self.all_detections = all_detections
 
     def main_process(self):
 
@@ -57,15 +60,13 @@ class VideoProcessor:
         self.get_video_parameters()
         self.initialize_video_writer()
 
-        all_detections = Detections()
-
         while self.cap.isOpened():
             ret, frame = self.cap.read()
             if not ret:
                 break
 
             detections = self.detect_object(frame)
-            all_detections.add_frame_to_detections(
+            self.all_detections.add_frame_to_detections(
                 detections, self.frame_number)
 
             frame_with_detection = self.draw_box_and_text(frame, detections)
@@ -76,7 +77,7 @@ class VideoProcessor:
         self.cap.release()
         self.out.release()
         cv2.destroyAllWindows()
-        all_detections.save_detections_in_json()
+        # self.all_detections.save_detections_in_json()
         return self.output_video_filename
 
     def initialize_video_writer(self):
@@ -134,81 +135,3 @@ class VideoProcessor:
                         cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
         return frame_with_rectangles
-
-
-# def video_processor(video_path: str,
-#                     confidence: float = 0.7,
-#                     iou: float = 0.5):
-#     video_filename, out, cap = get_video_config(video_path)
-#     frame_number = 1
-#     all_detections = Detections()
-
-#     while cap.isOpened():
-#         ret, frame = cap.read()
-#         if not ret:
-#             break
-
-#         detections = detect_object(frame, confidence, iou)
-#         all_detections.add_frame_to_detections(detections, frame_number)
-
-#         frame_with_detection = draw_box_and_text(frame, detections)
-
-#         out.write(frame_with_detection)
-#         frame_number += 1
-
-#     cap.release()
-#     out.release()
-#     cv2.destroyAllWindows()
-#     all_detections.save_detections_in_json()
-#     return video_filename
-
-
-# def get_video_config(video_path: str):
-#     frames_folder = 'output-videos'
-
-#     current_datetime = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-#     os.makedirs(frames_folder, exist_ok=True)
-#     video_filename = f'{current_datetime}.mp4'
-#     output_video_path = os.path.join(frames_folder, video_filename)
-
-#     cap = cv2.VideoCapture(video_path)
-#     fps = cap.get(cv2.CAP_PROP_FPS)
-#     frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-#     frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-#     fourcc = cv2.VideoWriter_fourcc(*'avc1')
-#     out = cv2.VideoWriter(output_video_path, fourcc, fps,
-#                           (frame_width, frame_height))
-#     return video_filename, out, cap
-
-
-# def detect_object(image_data: str,
-#                   confidence: float = 0.7,
-#                   iou: float = 0.5,
-#                   ):
-
-#     # image_path = os.path.join(current_directory, relative_image_path)
-#     original_img = Image.fromarray(cv2.cvtColor(image_data, cv2.COLOR_BGR2RGB))
-#     # original_img = Image.open(io.BytesIO(image_data)).convert('RGB')
-#     predictions = model(original_img, confidence, iou)
-#     detections = [p.to_dict() for p in predictions]
-#     return detections
-
-
-# def draw_box_and_text(frame: str, detections: list[dict]):
-#     # Make a copy of the frame to avoid modifying the original
-#     frame_with_rectangles = frame.copy()
-
-#     # Loop through each detection
-#     for detection in detections:
-#         class_name = detection['class_name']
-#         box = detection['box']
-#         left, top, width, height = box.values()
-
-#         # Draw rectangle on the frame
-#         cv2.rectangle(frame_with_rectangles, (left, top),
-#                       (left + width, top + height), (0, 255, 0), 2)
-#         # Add class name text
-#         cv2.putText(frame_with_rectangles, class_name, (left, top - 10),
-#                     cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-
-#     return frame_with_rectangles
